df1c8bb41adccf99152af69e02a11c27d0a0604e
kienau <oliverau93@gmail.com>
2023-11-29 18:11:25 -0500

    fixed tile

--- components/tile.vue
+++ components/tile.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="tile">
         <div class="tile__icon-wrapper">
-            <img class="tile__icon" :src="icon" alt="" />
+
         </div>
         <div class="tile__seperator">
             <span></span>
         </div>
         <p class="tile__description">
-            {{ description }}
+
         </p>
     </div>
 </template>
