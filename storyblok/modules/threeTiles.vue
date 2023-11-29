8dc55fde1d3d7fea741d31f4576ae29071a4a9b6
kienau <oliverau93@gmail.com>
2023-11-29 18:19:24 -0500

    update

--- storyblok/modules/threeTiles.vue
+++ storyblok/modules/threeTiles.vue
@@ -2,7 +2,7 @@
     <section v-editable="blok" class="three-tiles">
         <div class="three-tiles__illusion-bg"></div>
         <div class="three-tiles__inner">
-            <tile v-for="(tile, index) in blok.tiles" :key="index" :icon="tile.icon" :description="tile.description"/>
+
         </div>
     </section>
 </template>
