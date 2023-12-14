e8a5c20e1840f57c255f349f9cb600a79d4f51be
kienau <oliverau93@gmail.com>
2023-12-13 22:54:55 -0500

    finish the calendar modules

--- components/Calendar.vue
+++ components/Calendar.vue
@@ -5,7 +5,7 @@
                 <div class="calendar__prev" @click="previousMonth()">
                     <MacroArrowLeftWhite />
                 </div>
-                <div class="calendar__title"> <span class="calendar__title--month"></span> <span class="calendar__title--year"></span></div>
+                <div class="calendar__title"> <h5 class="calendar__title--month"></h5> <span></span> <h5 class="calendar__title--year"></h5></div>
                 <div class="calendar__next" @click="nextMonth()">
                     <MacroArrowRightWhite />
                 </div>
@@ -18,16 +18,20 @@
             <div class="calendar__day--container">
                 <div class="calendar__day" v-for="index in 42" :key="index" @click="highlightDay(index - 0)">
                     <span class="calendar__day-number"></span>
-                    <span class="calendar__day-event"></span>
+                    <ul class="calendar__day-event--list">
+                    </ul>
                 </div>
             </div>
         </div>
         <div class="calendar__legend">
             <h6> {{ $t('Legend:') }} </h6>
             <div class="calendar__legend--container">
-                <span class="calendar__legend--type">
-                    <span class="calendar__legend--color"></span>
-                </span>
+                <ul class="calendar__legend--type-list">
+                    <li class="calendar__legend--type-item"><span class="calendar__legend--color calendar__legend--color--courses"></span>Courses</li>
+                    <li class="calendar__legend--type-item"><span class="calendar__legend--color calendar__legend--color--workshops"></span>Workshops</li>
+                    <li class="calendar__legend--type-item"><span class="calendar__legend--color calendar__legend--color--events"></span>Events</li>
+                    <li class="calendar__legend--type-item"><span class="calendar__legend--color calendar__legend--color--celebrations"></span>Celebrations</li>
+                </ul>
             </div>
         </div>
         <div class="calendar__highlight">
@@ -35,7 +39,8 @@
                 <span class="calendar__selected-date"></span>
             </div>
             <ul class="calendar__highlight--event-list">
-                <li class="calendar__highlight--event-item" >
+                <li class="calendar__highlight--event-item" v-for="item in displayEvent" :class="'calendar__highlight--type-' + item.legends">
+                    {{ item.title }}, {{ item.startHours }} - {{ item.endHours }}
                 </li>
             </ul>
         </div>
@@ -46,20 +51,16 @@
     const storyblokApi = useStoryblokApi()
     const { data } = await storyblokApi.get('cdn/stories', {
         version: 'draft',
-        starts_with: 'event',
+        starts_with: 'events',
         is_startpage: false,
     })
 
-    let events
     const stories = data.stories
+    let eventList = []
 
-    events = stories.reduce((accumulator, event) => {
-        const eventTime = new Date(event.content.Date)
-        const eventDate = new Date(eventTime.toDateString())
-
-        accumulator[eventDate] = event.content
-        return accumulator
-    }, {})
+    stories.forEach((story) => {
+        eventList.push(story.content)
+    })
 
     const { t } = useI18n()
 
@@ -88,11 +89,11 @@
         t('months.December'),
     ])
 
+    let displayEvent = ref([]);
     const today = new Date()
     let currentMonth = today.getMonth()
     let currentYear = today.getFullYear()
-
-    const updateCalendar = (month, year, events) => {
+    const updateCalendar = (month, year) => {
         const theFirst = new Date()
         theFirst.setMonth(month)
         theFirst.setYear(year)
@@ -102,8 +103,8 @@
         const yearEl = document.querySelector('.calendar__title--year')
         const theFirstDayOfWeek = theFirst.getDay()
         const monthName = months.value[month]
-        monthEl.innerHTML = `${monthName}`
-        yearEl.innerHTML = `${year}`
+        monthEl.innerText = `${monthName}`
+        yearEl.innerText = `${year}`
         const daysInMonth = new Date(year, month + 1, 0).getDate()
         let dayCounter = 1
         for (let i = 0; i < daysElement.length; i++) {
@@ -112,14 +113,34 @@
 
             if (i >= theFirstDayOfWeek && dayCounter <= daysInMonth) {
                 const thisDate = new Date(year, month, dayCounter)
-                const eventName = day.querySelector('.calendar__day-event')
-                
-                if (events[thisDate]) {
-                    const event = events[thisDate]
-                    eventName.innerText = `* ${event.Title}`
-                } else {
-                    eventName.innerText = ''
-                }
+                const calendarEventList = day.querySelector('.calendar__day-event--list')
+                const calendarEventItems = calendarEventList.querySelectorAll('li')
+
+                calendarEventItems.forEach((item) => {
+                    item.remove()
+                })
+
+                eventList.forEach((event) => {
+                    const li = document.createElement('li')
+                    if (thisDate.getTime() === new Date(event.date).getTime()) {
+                        li.classList.add('calendar__dot')
+                        switch (event.legends) {
+                            case 'events':
+                                li.classList.add('calendar__dot--events')
+                                break
+                            case 'courses':
+                                li.classList.add('calendar__dot--courses')
+                                break
+                            case 'workshops':
+                                li.classList.add('calendar__dot--workshops')
+                                break
+                            case 'celebrations':
+                                li.classList.add('calendar__dot--celebrations')
+                                break
+                        }
+                        calendarEventList.appendChild(li);
+                    }
+                })
 
                 dayNumber.innerText = dayCounter
                 dayCounter++
@@ -149,11 +170,22 @@
                 formatDay = 'th'
         }
 
+        let currentDate = `${month} ${day + formatDay}, ${year}`
 
-        selectedDate.innerText = `${month} ${day + formatDay}, ${year}`
+        selectedDate.innerText = currentDate
+
+        eventList.forEach((event) => {
+            let eventDate = new Date(event.date)
+            if (eventDate.getTime() === new Date(`${month}-${day}-${year}`).getTime()) {
+                displayEvent.value.push(event)
+            }
+        })
     }
 
+    
+
     const highlightDay = (e) => {
+        displayEvent.value = [];
         const daysElement = document.querySelectorAll('.calendar__day')
         const selectedDate = document.querySelector('.calendar__selected-date')
         const selectedMonth = document.querySelector('.calendar__title--month').innerText
@@ -163,7 +195,7 @@
         })
         const index = e - 1
         const day = daysElement[index]
-        let selectedDay = day.innerText
+        let selectedDay = day.querySelector('.calendar__day-number').innerText
         const dayNumber = day.querySelector('.calendar__day-number')
         let formatDay;
 
@@ -181,35 +213,52 @@
         if (dayNumber.innerText != '') {
             day.classList.add('is-selected')
             selectedDate.innerText = `${selectedMonth} ${selectedDay + formatDay}, ${selectedYear}`
+            let date = new Date(`${selectedYear}-${selectedMonth}-${selectedDay}`)
+            eventList.forEach((event) => {
+                let eventDate = new Date(event.date)
+                if (eventDate.getTime() === date.getTime()) {
+                    displayEvent.value.push(event)
+                }
+            })
         }
     }
 
     const previousMonth = () => {
+        const daysElement = document.querySelectorAll('.calendar__day')
+
+        daysElement.forEach((day) => {
+            day.classList.remove('is-selected')
+        })
         if (currentMonth === 0) {
             currentMonth = 12
             currentYear--
         }
 
-        updateCalendar(--currentMonth, currentYear, events)
+        updateCalendar(--currentMonth, currentYear)
     }
 
     const nextMonth = () => {
+        const daysElement = document.querySelectorAll('.calendar__day')
+
+        daysElement.forEach((day) => {
+            day.classList.remove('is-selected')
+        })
+        
         if (currentMonth === 11) {
             currentMonth = -1
             currentYear++
         }
 
-        updateCalendar(++currentMonth, currentYear, events)
+        updateCalendar(++currentMonth, currentYear)
     }
 
     onMounted(() => {
         const loadEvents = async () => {
-            updateCalendar(currentMonth, currentYear, events)
+            updateCalendar(currentMonth, currentYear)
         }
 
         dateDisplaySelected()
         setTimeout(() => {
-            updateCalendar(currentMonth, currentYear, {})
             loadEvents()
         }, 0)
     })
